a99d99e176b2f41120d35d354c84a8b7ca4f9d3a
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2026-06-24 12:05:08 +0500

    feat: imported fonts and called demo shimmer loader screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
 import HomeScreen from '././src/screens/HomeScreen';
-import DemoScreen from './src/screens/DemoScreen';
+import ShimmerLoaderDemo from './src/screens/demos/ShimmerLoaderDemo';
 
 export type RootStackParamList = {
   Home: undefined;
-  Demo: { id: string; title: string };
+  ShimmerLoaderDemo: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const [loaded] = useFonts({
+    ClashDisplay_R: require('./assets/fonts/ClashDisplay-Regular.otf'),
+    ClashDisplay_M: require('./assets/fonts/ClashDisplay-Medium.otf'),
+    ClashDisplay_B: require('./assets/fonts/ClashDisplay-Bold.otf'),
+    Sora_R: require('./assets/fonts/Sora-Regular.ttf'),
+    Sora_M: require('./assets/fonts/Sora-Medium.ttf'),
+    Sora_B: require('./assets/fonts/Sora-Bold.ttf'),
+  })
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Demo" component={DemoScreen} />
+        <Stack.Screen name="ShimmerLoaderDemo" component={ShimmerLoaderDemo} />
       </Stack.Navigator>
     </NavigationContainer>
   );
